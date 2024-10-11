631c4c27777add248952bb725c773d178e9f2edf
Shehan <shehanjr99@gmail.com>
2024-10-11 19:39:32 +0530

    Added a css pre-set and MUI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import Login from './pages/login/login'
+
 
 function App() {
 
 
   return (
     <>
-
+      <Login />
     </>
   )
 }
---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '../../components/button/Button';
+import styles from './Login.module.css'
+
+const Login = () => {
+  return (
+    <div>
+      <p className={styles.text}>login</p>
+      <Button />
+    </div>
+  );
+}
+
+export default Login;
