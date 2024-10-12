d830d035a3ac56806abe95458d5acabd8202aab7
Shehan <shehanjr99@gmail.com>
2024-10-12 08:43:24 +0530

    Refactored the code

--- client/src/components/button/AuthButton.jsx
+++ client/src/components/button/AuthButton.jsx
@@ -1,20 +1,22 @@
 import Button from '@mui/material/Button'
 
+const styles = {
+    textDecoration: 'none',
+    textTransform: 'capitalize',
+    borderRadius: '3rem',
+    lineHeight: 1,
+    padding: '0.875rem',
+    fontSize: '1rem',
+    marginBlock: '0.5rem'
+}
+
 const AuthButton = ({ title }) => {
     return (
         <div>
             <Button
                 variant='contained'
                 fullWidth
-                sx={{
-                    textDecoration: 'none',
-                    textTransform: 'capitalize',
-                    borderRadius: '3rem',
-                    lineHeight: 1,
-                    padding: '0.875rem',
-                    fontSize: '1rem',
-                    marginBlock: '0.5rem'
-                }}
+                sx={styles}
             >{title}</Button>
         </div>
     );
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -2,6 +2,14 @@ import styles from './Login.module.css'
 import AuthButton from '../../components/button/AuthButton';
 import { Box, Container, TextField, Typography } from '@mui/material';
 
+const border = {
+  radius: {
+    '& .MuiOutlinedInput-root': {
+      borderRadius: '.5rem'
+    }
+  }
+}
+
 
 const Login = () => {
   return (
@@ -14,38 +22,35 @@ const Login = () => {
               Sign In
             </Typography>
           </Box>
-
-          <form onSubmit={''}>
-            <label htmlFor="email">Email</label>
-            <TextField
-              variant='outlined'
-              id='email'
-              size='small'
-              type='email'
-              fullWidth
-              className={styles.input}
-              sx={{
-                '& .MuiOutlinedInput-root': {
-                  borderRadius: '.5rem'
-                }
-              }} />
-
-            <label htmlFor="password">Your password</label>
-            <TextField
-              variant='outlined'
-              id='password'
-              size='small'
-              type='password'
-              fullWidth
-              className={styles.input}
-              sx={{
-                '& .MuiOutlinedInput-root': {
-                  borderRadius: '.5rem'
-                }
-              }} />
-
-            <AuthButton title={'Log In'}/>
-          </form>
+          <Box component='form' onSubmit={''} className={styles.form__container}>
+            <Box>
+              <label htmlFor="email">Email</label>
+              <TextField
+                required
+                variant='outlined'
+                id='email'
+                size='small'
+                type='email'
+                fullWidth
+                className={styles.input}
+                sx={border.radius} />
+            </Box>
+            <Box>
+              <label htmlFor="password">Your password</label>
+              <TextField
+                required
+                variant='outlined'
+                id='password'
+                size='small'
+                type='password'
+                fullWidth
+                className={styles.input}
+                sx={border.radius} />
+            </Box>
+            <Box>
+              <AuthButton title={'Log In'} />
+            </Box>
+          </Box>
         </Box>
       </Box>
     </Container>
